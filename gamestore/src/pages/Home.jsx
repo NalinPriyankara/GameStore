e392cd6a0b9422f803eaea4414eb05436a8a0ad9
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-05-15 21:23:44 +0530

    Homepage

--- gamestore/src/pages/Home.jsx
+++ gamestore/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.css';
 import GameSwiper from '../components/GameSwiper';
+import GameCard from '../components/GameCard';
 
 function Home({ games }) {
   return (
@@ -9,7 +10,7 @@ function Home({ games }) {
             <div className="row">
                 <GameSwiper games={games}/>
             </div>
-            <div className="row">
+            <div className="row mb-4 mt-4">
               <div className="col-lg-6">
                 <h2 className="sectionTitle">Games on promotion</h2>
               </div>
@@ -18,6 +19,13 @@ function Home({ games }) {
                   View more Games <i className="bi bi-arrow-right"></i>
                 </a>
               </div>
+            </div>
+            <div className="row">
+              {
+                games.slice(0, 4).map(game => (
+                  <GameCard key={game._id} game={game}/>
+                ))
+              }
             </div>
         </div>
     </section>
