d1bebdb46a5befd8e8d3de8f4e4e72a66176afa6
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-05-18 21:50:15 +0530

    category

--- gamestore/src/pages/Categories.jsx
+++ gamestore/src/pages/Categories.jsx
@@ -1,25 +1,55 @@
 import React, { useState } from 'react';
 import './categories.css';
 import filterListData from '../data/filterListData';
+import GameCard from '../components/GameCard';
 
 function Categories({ games, reference }) {
+  const [data, setData] = useState(games);
+
   const [filters, setFilters] = useState(filterListData);
+  const handleFilterGames = category => {
+    setFilters (
+      filters.map(filter => {
+        filter.active = false;
+        if (filter.name === category) {
+          filter.active = true;
+        }
+        return filter;
+      })
+    );
+  }
   return (
     <section id="categories" className='categories' ref={reference}>
       <div className="container-fluid mt-2">
         <div className="row">
-          <div className="col-lg-8">
+          <div className="col-lg-8 d-flex align-items-center justify-content-start">
             <ul className="filters">
               {filters.map(filter => (
-                <li key = {filter._id}>{filter.name}</li>
+                <li 
+                key = {filter._id} 
+                className={`${filter.active ? 'active' : undefined}`}
+                onClick={() => handleFilterGames(filter.name)}
+                >
+                  {filter.name}
+                </li>
               ))}
             </ul>
           </div>
-          <div className="col-lg-4"></div>
+          <div className="col-lg-4 d-flex align-items-center justify-content-end">
+            <div className="search">
+              <i class="bi bi-search"></i>
+              <input type="text" name="search" placeholder='Search' />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {games.map(game=>(
+            <GameCard key={game._id} game={game} />
+          ))}
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
